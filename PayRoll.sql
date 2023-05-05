46d246bd0248ce2f32378a2a5bd50bf864dc6b17
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-05-05 07:56:01 +0530

    Added : Find Salary And Date

--- PayRoll.sql
+++ PayRoll.sql
@@ -1,4 +1,4 @@
------------------------------ Uc4 -------------------------
-SELECT * FROM payroll_service.employee_payroll;
+----------------------------- Uc5 -------------------------
 insert into employee_payroll(name,salay,start_date) values('Virat',100000.00,'2023-04-05'),('Rohit',350000.00,'2035-04-02'),('Dhoni',450000.00,'2023-05-06');
-select * from employee_payroll;
+select  salay  as "Salary" from employee_payroll where name ='virat' limit 2;
+select * from employee_payroll where cast('2023-04-05' As Date) AND  Date (now());
