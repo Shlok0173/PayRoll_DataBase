f6cec7cd21abca644a40b7bd183816b826bc5e26
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-05-05 07:44:40 +0530

    Show All Data

--- PayRoll.sql
+++ PayRoll.sql
@@ -1,4 +1,4 @@
------------------------------ Uc3 -------------------------
+----------------------------- Uc4 -------------------------
 SELECT * FROM payroll_service.employee_payroll;
 insert into employee_payroll(name,salay,start_date) values('Virat',100000.00,'2023-04-05'),('Rohit',350000.00,'2035-04-02'),('Dhoni',450000.00,'2023-05-06');
 select * from employee_payroll;
