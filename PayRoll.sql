3c8007c3d86cc48aa57101fd4b845b4a390fa7a7
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-05-05 07:04:02 +0530

    Added : Create Table

--- PayRoll.sql
+++ PayRoll.sql
@@ -3,3 +3,8 @@ show databases;
 --------------- Uc1---------------
 use payroll_service;
 show databases;
+
+------------------------- Uc2 -----------------------------
+create table employee_payroll(id int(11) auto_increment not null , name varchar(200) not null , salary double not null,start_date date not null);
+desc employee_payroll;
+show databases;
